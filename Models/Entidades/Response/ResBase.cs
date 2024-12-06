a7ea062021e69eab3897aaab27f134a6523fa1ee
ale <alemccr@gmail.com>
2024-12-05 19:15:43 -0600

    Se añaden carpetas de request y response

---
+++ Models/Entidades/Response/ResBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Silicon.Models.Entidades
+{
+    public class ResBase
+    {
+        public bool resultado { get; set; }
+        public List<String> errores { get; set; }
+    }
+}
