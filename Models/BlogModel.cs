276bb38e8c1c74fe36efd21552937ad6f1e88da0
Ariadna <ariadna.fuentes@ulatina.net>
2024-12-15 09:32:52 -0600

    .

--- Models/BlogModel.cs
+++ Models/BlogModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using static Silicon.Models.AccountModel;
+using static Silicon.Models.BlogModel;
 using System.Web.UI.WebControls;
 using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -13,44 +13,51 @@ namespace Silicon.Models
 {
     public class BlogModel
     {
-        public ComentarioModel Comentario { get; set; }
+        public PeliculaModel Pelicula { get; set; }
 
         public BlogModel()
         {
-            Comentario = new ComentarioModel();
+            Pelicula = new PeliculaModel();
         }
 
-        public class ComentarioModel
+        public class PeliculaModel
         {
             [Key]
             [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-            [Display(Name = "ID de Comentario")]
-            public int ReviewId { get; set; }
-
-            [Required(ErrorMessage = "Por favor selecciona una película.")]
             [Display(Name = "ID de Película")]
             public int MovieId { get; set; }
 
-            [Required(ErrorMessage = "Por favor escribe un comentario.")]
-            [Display(Name = "Comentario")]
-            [StringLength(500, MinimumLength = 5, ErrorMessage = "El comentario debe tener entre 5 y 500 caracteres.")]
-            public string Comment { get; set; }
+            [Required(ErrorMessage = "Por favor ingresa el título de la película.")]
+            [Display(Name = "Título")]
+            public string Title { get; set; }
 
-            [Required(ErrorMessage = "Por favor califica la película.")]
-            [Range(1, 5, ErrorMessage = "La calificación debe estar entre 1 y 5 estrellas.")]
-            [Display(Name = "Calificación")]
-            public int Rating { get; set; }
+            [Required(ErrorMessage = "Por favor ingresa la sinopsis de la película.")]
+            [Display(Name = "Sinopsis")]
+            [StringLength(1000, MinimumLength = 10, ErrorMessage = "La sinopsis debe tener entre 10 y 1000 caracteres.")]
+            public string Synopsis { get; set; }
 
-            [Required(ErrorMessage = "Debes estar registrado para dejar un comentario.")]
-            [Display(Name = "ID de Usuario")]
-            public int UserId { get; set; }
+            [Required(ErrorMessage = "Por favor ingresa la duración de la película.")]
+            [Display(Name = "Duración")]
+            [Range(1, 300, ErrorMessage = "La duración debe ser entre 1 y 300 minutos.")]
+            public int Duration { get; set; }  
 
+            [Required(ErrorMessage = "Por favor ingresa la fecha de estreno.")]
+            [Display(Name = "Fecha de Estreno")]
+            [DataType(DataType.Date)]
+            public DateTime ReleaseDate { get; set; }
 
+            [Required(ErrorMessage = "Por favor ingresa el nombre del director.")]
+            [Display(Name = "Director")]
+            public string Director { get; set; }
 
-            [Display(Name = "Fecha de Publicación")]
-            [DataType(DataType.DateTime)]
-            //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-            public System.DateTime PublicationDate { get; set; };
+            [Required(ErrorMessage = "Por favor selecciona el género de la película.")]
+            [Display(Name = "Género")]
+            public string Genre { get; set; }
+
+            [Display(Name = "Imagen de la Película")]
+            public string ImageUrl { get; set; }
+
+            public ICollection<ComentarioModel> Comentarios { get; set; }
         }
     }
 }
